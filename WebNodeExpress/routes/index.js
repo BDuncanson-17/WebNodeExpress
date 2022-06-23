02ce3cc53bd06ba043babcf01170155404e5a4d2
Bryan Duncanson <bduncanson@madisoncollege.edu>
2022-06-22 20:13:30 -0500

    Added some minor things

--- WebNodeExpress/routes/index.js
+++ WebNodeExpress/routes/index.js
@@ -2,9 +2,17 @@
 var express = require('express');
 var router = express.Router();
 
+var getData = function () {
+    var data = {
+        'item1': './WebNodeExpress\public\images\France_1.jpg',
+        'item2': './WebNodeExpress\public\images\France_2.jpg',
+        'item3': './WebNodeExpress\public\images\France_3.jpg',
+    }
+    return data;
+}
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express', "data": getData() });
 });
 
 module.exports = router;
